2e3860b740a726ecb29d86f98fceee7269cb55a2
hosein ladmakhi <ladmakhi81@gmail.com>
2025-02-17 08:17:29 +0330

    feat: create migration for role and user

---
+++ migrations/20250216105418_create_role_table.down.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS "_roles" (
+    "id" BIGSERIAL PRIMARY KEY,
+    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP DEFAULT NULL,
+    "created_by_id" INT,
+    "lock" BOOLEAN DEFAULT false,
+    "permissions" VARCHAR []
+)

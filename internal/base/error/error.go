0cf966df20506789343da7789b04d128414b8767
hosein ladmakhi <ladmakhi81@gmail.com>
2025-02-19 10:30:28 +0330

    feat: add model of custom error

---
+++ internal/base/error/error.go
@@ -0,0 +1,39 @@
+package baseerror
+
+import "net/http"
+
+type ClientErr struct {
+	Message    string `json:"message"`
+	StatusCode uint   `json:"statusCode"`
+}
+
+type ClientValidationErr struct {
+	ClientErr
+	Errors any `json:"errors"`
+}
+
+type ServerErr struct {
+	Message  string `json:"message"`
+	Location string `json:"location"`
+}
+
+func NewClientErr(message string, statusCode uint) ClientErr {
+	return ClientErr{
+		Message:    message,
+		StatusCode: statusCode,
+	}
+}
+
+func NewClientValidationErr(errors any) ClientValidationErr {
+	return ClientValidationErr{
+		Errors:    errors,
+		ClientErr: NewClientErr("Invalid Input Body", http.StatusBadRequest),
+	}
+}
+
+func NewServerErr(message, location string) ServerErr {
+	return ServerErr{
+		Message:  message,
+		Location: location,
+	}
+}
